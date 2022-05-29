e08eb685957f0d480959a469cafde166d59088a0
Developer LuisF3 <developerluisfernando3@gmail.com>
2022-05-28 19:27:55 -0500

    [MOD, index.tsx][ADD, appContent]

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,30 @@ import type { NextPage } from "next";
 import Head from "next/head";
 
 // Compponents
-import Layout from "../components/layout/layout";
+import Layout, { siteTitle } from "../components/layout/layout";
+import CoverPage from "../components/coverPage/coverPage";
+import CoverPageHome from "../components/coverPage/coverPageHome";
+import CoverTitle from "../components/coverPage/coverPageTitle";
+import AppContainer from "../components/appContainer/appContainer";
+import HomePresentation from "../components/homePresentation/homePresentation";
+import JavascriptHome from "../components/javascriptHome/javascriptHome";
 
+// Main component
 const Home: NextPage = () => {
   return (
     <Layout>
       <Head>
-        <title>Home - Developer LuisF3</title>
+        <title>Home - {siteTitle}</title>
       </Head>
+      <CoverPage>
+        <CoverPageHome>
+          <CoverTitle />
+        </CoverPageHome>
+      </CoverPage>
+      <AppContainer>
+        <HomePresentation />
+        <JavascriptHome />
+      </AppContainer>
     </Layout>
   );
 };
