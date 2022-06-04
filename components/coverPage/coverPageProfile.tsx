d54b7d8e399d1ff25a49eb4ace1f6cd6d3fd1f04
Developer LuisF3 <developerluisfernando3@gmail.com>
2022-06-03 22:54:15 -0500

    [ADD, coverPageProfile][ADD, component]

---
+++ components/coverPage/coverPageProfile.tsx
@@ -0,0 +1,55 @@
+// React Js
+import * as React from "react";
+
+// Next-auth
+import { useSession } from "next-auth/react";
+
+// Material Ui
+import { styled } from "@mui/material/styles";
+import Skeleton from "@mui/material/Skeleton";
+import Stack from "@mui/material/Stack";
+
+// Styled
+const CoverPageImg = styled("div")(({ theme }) => ({
+  minWidth: "100%",
+  minHeight: "100vh",
+  backgroundImage:
+    "-webkit-linear-gradient(rgba(25,25,25,.9), rgba(0,0,0,.1) ), url(/img/coverPage/hacker.jpg)",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  backgroundAttachment: "fixed",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+// Interface
+interface Props {
+  children: React.ReactNode;
+}
+
+// Main component
+function CoverPageProfile({ children }: Props) {
+  const { data: session, status } = useSession();
+  return (
+    <React.Fragment>
+      {status === "loading" && (
+        <Stack spacing={1}>
+          <Skeleton
+            variant="rectangular"
+            style={{
+              width: "100%",
+              height: "100vh",
+            }}
+            color="green"
+          />
+        </Stack>
+      )}
+      {status === "authenticated" && <CoverPageImg>{children}</CoverPageImg>}
+      {status === "unauthenticated" && <CoverPageImg>{children}</CoverPageImg>}
+    </React.Fragment>
+  );
+}
+
+export default CoverPageProfile;
