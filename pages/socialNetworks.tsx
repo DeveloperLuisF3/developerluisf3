0169a23a2ee645697eb616f2eb9d9812def6c23d
Developer LuisF3 <developerluisfernando3@gmail.com>
2022-06-04 20:59:23 -0500

    [ADD, socialNetwork.tsx][ADD, socialNetwork-page]

---
+++ pages/socialNetworks.tsx
@@ -0,0 +1,16 @@
+// NextJs
+import Head from "next/head";
+
+// Components
+import Layout from "../components/layout/layout";
+
+let SocialNetworks = () => {
+  return (
+    <Layout>
+      <Head>Redes Sociales - DeveloperLuisF3</Head>
+      hola wey
+    </Layout>
+  );
+};
+
+export default SocialNetworks;
