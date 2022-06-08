60f77b6747a00c35e699ba95945d7bf6b4dd1c3a
Developer LuisF3 <developerluisfernando3@gmail.com>
2022-06-08 14:10:14 -0500

    [ADD, socialNetwork.tsx][ADD, page]

--- pages/socialNetworks.tsx
+++ pages/socialNetworks.tsx
@@ -1,14 +1,26 @@
 // NextJs
+import type { NextPage } from "next";
 import Head from "next/head";
 
 // Components
 import Layout from "../components/layout/layout";
+import CoverPage from "../components/coverPage/coverPage";
+import CoverPageSocial from "../components/coverPage/coverPageSocial";
+import SocialPresentation from "../components/socialPresentation/socialPresentation";
+import AppContainer from "../components/appContainer/appContainer";
+import SocialComponent from "../components/socialComponent/socialComponent";
 
-let SocialNetworks = () => {
+// Main component
+const SocialNetworks: NextPage = () => {
   return (
     <Layout>
-      <Head>Redes Sociales - DeveloperLuisF3</Head>
-      hola wey
+      <Head>
+        <title>Redes Sociales - DeveloperLuisF3</title>
+      </Head>
+      <SocialPresentation />
+      <AppContainer>
+        <SocialComponent />
+      </AppContainer>
     </Layout>
   );
 };
